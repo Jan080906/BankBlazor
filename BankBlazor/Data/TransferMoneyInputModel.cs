f8466830fd8351bbddad00b2cee6a252a9ef4289
Jan eiei <jan314052gmail.com@Jans-Mac-mini.local>
2021-06-12 21:05:46 +0700

    able to transfer

---
+++ BankBlazor/Data/TransferMoneyInputModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BankBlazor.Data
+{
+    public class TransferMoneyInputModel
+    {
+        public int FromAccountNumber { get; set; }
+        public int ToAccountNumber { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
